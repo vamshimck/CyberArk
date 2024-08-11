3d0c9761419544415daa37054a39576108046af2
vamshimck <vamshirao.y@gmail.com>
2024-08-11 16:40:32 -0500

    Update entrypoint.sh

--- my-custom-action/entrypoint.sh
+++ my-custom-action/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Conjur Secret Retrieval for GitHub Action conjur-action
 
-stages:
-  - read-secrets
-  - build
-  - test
-  - deploy
-
-read-secrets:
-  tags:
-    - jwt
-  stage: read-secrets
-  script:
-  - echo " Reading Secrets From Conjur"
-  - 'export TOKEN=$(curl -k --header "Content-Type: text/plain" --header "Accept-Encoding: base64" --request POST "https://mckesson.secretsmgr.cyberark.cloud/aauthn-jwt/github/conjur/authenticate")'
-  - echo "Here is the access token"
-  - echo {TOKEN}
+jobs:
+  read-secrets:
+    runs-on: self-hosted
+    steps:
+      - name: Read Secrets From Conjur
+        run: |
+          echo "Reading Secrets From Conjur"
+          export TOKEN=$(curl -k --header "Content-Type: text/plain" --header "Accept-Encoding: base64" --request POST "https://mckesson.secretsmgr.cyberark.cloud/aauthn-jwt/github/conjur/authenticate")
+          echo "Here is the access token"
+          echo "${TOKEN}"
+        env:
+          # You can add environment variables here if needed
+          # For example, to use GitHub Secrets:
+          # CONJUR_URL: ${{ secrets.CONJUR_URL }}
+          # CONJUR_AUTH: ${{ secrets.CONJUR_AUTH }}
